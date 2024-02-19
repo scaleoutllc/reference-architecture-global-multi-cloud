69f38f4efe4f33b95591af1e20e1678d6a9f27bb
Tyler Kellen <tyler@goingslowly.com>
2024-02-20 00:03:07 +1300

    add initial provider and region of dummy service

---
+++ infrastructure/aws/team/dev-us/remote-states.tf
@@ -0,0 +1,14 @@
+data "terraform_remote_state" "routing" {
+  backend = "remote"
+
+  config = {
+    organization = "scaleout"
+    workspaces = {
+      name = "routing"
+    }
+  }
+}
+
+locals {
+  routing = data.terraform_remote_state.routing.outputs.team-dev
+}
