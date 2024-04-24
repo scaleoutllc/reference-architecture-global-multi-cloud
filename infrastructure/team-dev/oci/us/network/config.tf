ce1e2ff5843d750aac5302f70c1f28deb1138b93
Tyler Kellen <tyler@goingslowly.com>
2024-04-24 23:44:23 +1000

    test oci in terraform cloud

--- infrastructure/team-dev/oci/us/network/config.tf
+++ infrastructure/team-dev/oci/us/network/config.tf
@@ -36,14 +36,6 @@ terraform {
 }
 
 provider "oci" {
-  region       = "us-chicago-1"
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.private_key
+  region = "us-chicago-1"
+  #config_file_profile = "scaleout" # uncomment for local applies
 }
-
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key" {}
